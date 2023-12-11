7c7d1c6190fe36e96de89513b9e3c21e470a3fe0
duxiaolong <du_xiaolong@hotmail.com>
2023-12-11 15:18:04 +0800

    水印相机添加位置

--- app/src/main/java/com/dxl/androidscaffold/ui/waterMarkerCamera/WaterMarkerCameraActivity.kt
+++ app/src/main/java/com/dxl/androidscaffold/ui/waterMarkerCamera/WaterMarkerCameraActivity.kt
@@ -1,6 +1,7 @@
 package com.dxl.androidscaffold.ui.waterMarkerCamera
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -14,9 +15,16 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.baidu.location.BDLocation
+import com.dxl.androidscaffold.baidu.BaiduLocation
 import com.dxl.androidscaffold.databinding.ActivityWaterMarkerCameraBinding
 import com.dxl.androidscaffold.databinding.ViewWaterMarkerBinding
+import com.dxl.androidscaffold.ui.waterMark.WaterMarkDrawable.Companion.setWaterMark
 import java.io.InputStream
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 /**
  * 水印相机
@@ -28,6 +36,8 @@ class WaterMarkerCameraActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityWaterMarkerCameraBinding
 
     private var cameraUri: Uri? = null
+
+    private var currentLocation: BDLocation? = null
 
     private fun createCameraUri(): Uri? {
         return contentResolver.insert(
@@ -60,6 +70,7 @@ class WaterMarkerCameraActivity : AppCompatActivity() {
 
         }
 
+    @SuppressLint("SetTextI18n")
     private fun addWaterMarker() {
         val cameraUri = cameraUri ?: return
         contentResolver.openInputStream(cameraUri).use { inputStream: InputStream? ->
@@ -72,12 +83,39 @@ class WaterMarkerCameraActivity : AppCompatActivity() {
             val canvas = Canvas(createBitmap)
             canvas.drawBitmap(sourceBitmap, 0f, 0f, null)
             val waterMarkerBinding = ViewWaterMarkerBinding.inflate(layoutInflater)
+
+            //时间
+            val date = Date()
+            waterMarkerBinding.tvTime.text =
+                SimpleDateFormat("HH:mm", Locale.getDefault()).format(date)
+            //具体时间
+            val weeks =
+                arrayOf("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六")
+            val week = weeks[Calendar.getInstance()[Calendar.DAY_OF_WEEK] - 1]
+            waterMarkerBinding.tvTimeDetail.text =
+                SimpleDateFormat(
+                    "yyyy-MM-dd HH:mm:ss",
+                    Locale.getDefault()
+                ).format(date) + " " + week
+            //地理位置
+            waterMarkerBinding.tvAddress.text =
+                currentLocation.address()
+            waterMarkerBinding.tvLocation.text =
+                "经度：${currentLocation?.longitude ?: 0.0}  纬度：${currentLocation?.latitude ?: 0.0}"
+            //类型
+            waterMarkerBinding.tvType.text = "打卡"
+
             val waterMarkerView = waterMarkerBinding.root
             waterMarkerView.measure(
                 View.MeasureSpec.makeMeasureSpec(sourceBitmap.width, View.MeasureSpec.EXACTLY),
                 View.MeasureSpec.makeMeasureSpec(sourceBitmap.height, View.MeasureSpec.AT_MOST)
             )
-            waterMarkerView.layout(0,0,waterMarkerView.measuredWidth,waterMarkerView.measuredHeight)
+            waterMarkerView.layout(
+                0,
+                0,
+                waterMarkerView.measuredWidth,
+                waterMarkerView.measuredHeight
+            )
             canvas.save()
 
             val matrix = Matrix()
@@ -93,6 +131,14 @@ class WaterMarkerCameraActivity : AppCompatActivity() {
         }
     }
 
+    private fun BDLocation?.address(): String {
+        val poi = this?.poiList?.firstOrNull()
+        if (poi != null) {
+            return poi.addr + poi.name
+        }
+        return this?.addrStr ?: "未获取到位置"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(
@@ -101,6 +147,15 @@ class WaterMarkerCameraActivity : AppCompatActivity() {
         viewBinding.btnCamera.setOnClickListener {
             cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
+        startLocation()
+    }
+
+    private fun startLocation() {
+        BaiduLocation.Builder(this)
+            .setOnceLocation(false)
+            .setOnReceiveLocation {
+                currentLocation = it
+            }.start()
     }
 
 }
